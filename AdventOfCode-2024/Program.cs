226081824ae9950014b0d52789244f4beff8fe75
Zach <53870574+Jadefire16@users.noreply.github.com>
2024-12-02 00:51:35 -0500

    Updated For Part 2
    Updated to add another method to support Part 2 of Day 1

--- AdventOfCode-2024/Program.cs
+++ AdventOfCode-2024/Program.cs
@@ -11,6 +11,12 @@ public class Program
 {
     private static void Main(string[] args)
     {
+        //PartOne();
+        PartTwo();
+    }
+
+    private static void PartOne()
+    {
         List<int> left = new();
         List<int> right = new();
 
@@ -21,7 +27,7 @@ public class Program
 
         using StreamReader reader = new(resources);
 
-        while (reader.ReadLine() is {} line)
+        while (reader.ReadLine() is { } line)
         {
             ReadOnlySpan<char> span = line.AsSpan();
             int lastSpace = line.LastIndexOf(" ", StringComparison.Ordinal);
@@ -50,5 +56,63 @@ public class Program
         }
         Console.WriteLine($"Final Accumulated Value: {finalValue}");
     }
+
+    private static void PartTwo()
+    {
+
+        Dictionary<int, Key> values = new();
+
+        string root = AppDomain.CurrentDomain.BaseDirectory;
+        string resources = Path.Combine(root, "Resources", "input.txt");
+        if (!File.Exists(resources))
+            throw new Exception($"Missing Input File At Path: {resources}");
+
+        using StreamReader reader = new(resources);
+
+        while (reader.ReadLine() is { } line)
+        {
+            ReadOnlySpan<char> span = line.AsSpan();
+            int lastSpace = line.LastIndexOf(" ", StringComparison.Ordinal);
+
+            try
+            {
+                int lValue = int.Parse(span[..(lastSpace - 2)]);
+                int rValue = int.Parse(span[(lastSpace + 1)..]);
+                
+                if(!values.TryAdd(lValue, new Key(1, 0)))
+                    values[lValue].KeyCount++;
+
+                if(!values.TryGetValue(rValue, out Key? value))
+                    values.Add(rValue, new Key(0, 1));
+                else
+                    value.ValueCount++;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to parse values", ex);
+            }
+        }
+
+        int accumulator = 0;
+
+        foreach (var value in values)
+        {
+            accumulator += ((value.Key * value.Value.KeyCount) * value.Value.ValueCount);
+        }
+
+        Console.WriteLine($"Final Accumulated Value: {accumulator}");
+    }
+    private record Key
+    {
+        public int KeyCount { get; set; }
+        public int ValueCount { get; set; }
+
+        public Key(int keyCount, int valueCount)
+        {
+            KeyCount = keyCount;
+            ValueCount = valueCount;
+        }
+    }
 }
-// Final answer: 1646452
+// Final answer part one: 1646452
+// Final answer part two: 23609874
