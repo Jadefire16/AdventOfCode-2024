4e94ce3e7c46be990b1e093479f8fc5a5cb2633e
Zach <53870574+Jadefire16@users.noreply.github.com>
2024-12-02 02:22:21 -0500

    Completed Day 2 Part 2
    Completed Day 2 Part 2 of AOC
    (Took less than a minute surprisingly)

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -1,10 +1,16 @@
-﻿
+﻿using AdventOfCode_2024.Common;
 
 namespace AdventOfCode_2024.DayTwo;
 
 internal class Program
 {
     private static void Main(string[] args)
+    {
+        // Part One();
+        // PartTwo();
+    }
+
+    private static void PartTwo()
     {
         string root = AppDomain.CurrentDomain.BaseDirectory;
         string resources = Path.Combine(root, "Resources", "input.txt");
@@ -21,7 +27,63 @@ internal class Program
 
         while (reader.ReadLine() is { } line)
         {
-            int count = Parse(line, buffer);
+            int count = Parsing.ParseNumber(line, buffer, ' ');
+
+            bool isDecrementing = buffer[0] > buffer[1];
+            int failures = 0;
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                int difference = buffer[i + 1] - buffer[i];
+                if (difference == 0)
+                {
+                    failures++;
+                }
+                else if (isDecrementing)
+                {
+                    if (buffer[i] >= buffer[i + 1] && difference >= -3)
+                        continue;
+
+                    failures++;
+                }
+                else
+                {
+                    if (buffer[i] <= buffer[i + 1] && difference <= 3)
+                        continue;
+
+                    failures++;
+                }
+            }
+
+            if (failures <= 1)
+                safeReports++;
+            else
+                unsafeReports++;
+
+            buffer.Fill(0);
+        }
+
+        Console.WriteLine($"Safe Reports: {safeReports}\nInvalid Reports: {unsafeReports}");
+    }
+
+    private static void PartOne()
+    {
+        string root = AppDomain.CurrentDomain.BaseDirectory;
+        string resources = Path.Combine(root, "Resources", "input.txt");
+        if (!File.Exists(resources))
+            throw new Exception($"Missing Input File At Path: {resources}");
+
+        using StreamReader reader = new(resources);
+
+        Span<int> buffer = stackalloc int[16];
+        buffer.Fill(0); // just sanity checking in case the allocated memory isn't cleared
+
+        int safeReports = 0;
+        int unsafeReports = 0;
+
+        while (reader.ReadLine() is { } line)
+        {
+            int count = Parsing.ParseNumber(line, buffer, ' ');
 
             bool isDecrementing = buffer[0] > buffer[1];
             bool isValid = true;
@@ -64,34 +126,5 @@ internal class Program
         }
 
         Console.WriteLine($"Safe Reports: {safeReports}\nInvalid Reports: {unsafeReports}");
-    }
-
-    private static int Parse(string line, Span<int> buffer)
-    {
-        ReadOnlySpan<char> span = line.AsSpan();
-
-        int start = 0, count = 0;
-
-        while (start < span.Length && count < buffer.Length)
-        {
-            int end = span[start..].IndexOf(' ');
-            ReadOnlySpan<char> slice;
-
-            if (end == -1)
-            {
-                slice = span[start..];
-                start = span.Length;
-            }
-            else
-            {
-                slice = span[start..(start + end)];
-                start += end + 1;
-            }
-
-            if (int.TryParse(slice, out int number))
-                buffer[count++] = number;
-        }
-
-        return count;
     }
 }
