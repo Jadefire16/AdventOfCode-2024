6cdeac07f8f7b217695f8be5a051e158ce72e57e
Zach <53870574+Jadefire16@users.noreply.github.com>
2024-12-02 02:18:49 -0500

    Added Commons Project
    Added a common project for all potentially re-usable code through AOC 2024

---
+++ Common/Parsing.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode_2024.Common;
+public class Parsing
+{
+    /// <summary>
+    /// Parses a sequence of numbers from a string given a single delimiter
+    /// </summary>
+    /// <param name="line"></param>
+    /// <param name="buffer"></param>
+    /// <param name="delimiter"></param>
+    /// <returns></returns>
+    public static int ParseNumber(string line, Span<int> buffer, char delimiter)
+    {
+        ReadOnlySpan<char> span = line.AsSpan();
+
+        int start = 0, count = 0;
+
+        while (start < span.Length && count < buffer.Length)
+        {
+            int end = span[start..].IndexOf(delimiter);
+            ReadOnlySpan<char> slice;
+
+            if (end == -1)
+            {
+                slice = span[start..];
+                start = span.Length;
+            }
+            else
+            {
+                slice = span[start..(start + end)];
+                start += end + 1;
+            }
+
+            if (int.TryParse(slice, out int number))
+                buffer[count++] = number;
+        }
+
+        return count;
+    }
+}
